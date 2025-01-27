f303d7ce2ff2e003d7148081c7ef219445a8926d
tommasotode <tommasotodescato@gmail.com>
2025-01-27 11:40:15 +0100

    crypto05

--- crypto/crypto05.py
+++ crypto/crypto05.py
@@ -1,6 +1,10 @@
 cifrato = bytes.fromhex("104e137f425954137f74107f525511457f5468134d7f146c4c")
 print(cifrato, len(cifrato))
 
-
 def xor(a, b):
   return bytes([x^y for x,y in zip(a,b)])
+
+for i in range(256):
+  chiave = bytes([i]*len(cifrato))
+  decifrato = xor(cifrato, chiave)
+  print(decifrato.decode(errors='ignore'))
