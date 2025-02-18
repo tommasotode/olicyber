4a2c971341038a498c061099428b121f8fd7acd9
tommasotode <tommasotodescato@gmail.com>
2025-02-18 16:26:57 +0100

    youcompleteme

---
+++ camp/youcompleteme/solve.py
@@ -0,0 +1,6 @@
+with open('camp/complete/words.txt') as f:
+    words = list(sorted([word.strip().encode('utf-8') for word in f.readlines()]))
+
+for w in words:
+    if len(w) == 13 and w[-8] == w[-9] and w[-6] == w[-10] and w[-10] == w[-2]:
+        print(w)
