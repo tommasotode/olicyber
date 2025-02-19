207df80ef9ff70878a62ede417f6aad87b153746
tommasotode <tommasotodescato@gmail.com>
2025-02-19 12:17:02 +0100

    keygenerator

---
+++ crypto/keygenerator/solve.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from hashlib import sha256
+from datetime import datetime
+import random
+
+# 2021-03-21 17:37:40
+
+def int_to_bytes(x):
+    return x.to_bytes((x.bit_length() + 7) // 8, 'big')
+
+def int_from_bytes(xbytes):
+    return int.from_bytes(xbytes, 'big')
+
+def generate_secure_key():
+    ts = int(datetime.timestamp(datetime(2021, 3, 21, 17, 37, 40)))
+    h = sha256(int_to_bytes(ts)).digest()
+
+    seed = int_from_bytes(h[32:])
+    key = h[:32]
+    
+    random.seed(seed)
+    for _ in range(32):
+        key += bytes([random.randint(0, 255)])
+
+    return key
+
+key = generate_secure_key().hex()
+print(key)
+
+# xor(key, flag.enc)
